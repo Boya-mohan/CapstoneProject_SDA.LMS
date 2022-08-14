2f3339dc39c852887e3fe9a371638200b1e05925
Boya-mohan <81395548+Boya-mohan@users.noreply.github.com>
2022-08-14 10:55:08 +0530

    Create MedicineCategory.cs

---
+++ Data/Enums/MedicineCategory.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mohan_CapstoneProject_SDA.LMS.Data
+{
+    public enum MedicineCategory
+    {
+        MumAndBaby,
+        BeautyCare,
+        PersonalCare,
+        MedicineAndTreatment,
+        Vitamins
+    }
+}
