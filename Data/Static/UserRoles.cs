5242cb5d091e2a9ea5ad9aab26d2d5e3bc8a2e2d
Boya-mohan <81395548+Boya-mohan@users.noreply.github.com>
2022-08-14 11:00:30 +0530

    Create UserRoles.cs

---
+++ Data/Static/UserRoles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mohan_CapstoneProject_SDA.LMS.Data.Static
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
